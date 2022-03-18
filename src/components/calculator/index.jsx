c0c98b16f7ac3bd081de994556b9a1dd3521356c
iapposki <konark981999@gmail.com>
2022-03-19 01:19:28 +0530

    added various functions, added history, input area revamped, basic scientific calculator complete

--- src/components/calculator/index.jsx
+++ src/components/calculator/index.jsx
@@ -1,67 +1,111 @@
 import React from "react";
-import './index.css'
+import './index.css';
+import {evaluate, string} from 'mathjs';
 
 class Calculator extends React.Component{
     constructor(props){
         super(props)
-        this.input = {
-            equation: ""
+        this.state = {
+            equation: "",
+            history:[]
         };
         this.handleEquationChange = this.handleEquationChange.bind(this);
-        this.handleClick = this.handleClick.bind(this)
-        
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSolve = this.handleSolve.bind(this);
+        this.updateState = this.updateState.bind(this);
+        this.clearEquation = this.clearEquation.bind(this);
+        this.bsEquation = this.bsEquation.bind(this);
+    }
+
+    updateState(newState){
+        this.setState(newState)
     }
 
     handleEquationChange(e){
-        // console.log(e.target.value)
-        this.input.equation = e.target.value
-        // console.log(this.input.equation)
+        // console.log(e.target.name)
+        var copyHistory = [...this.state.history];
+        this.updateState({equation:e.target.name,history:copyHistory})
+        // console.log(this.state.equation)
     }
     
     handleClick(e){
-        equation.concat(e.target.equation)
-        console.log(equation)
-        console.log(e.target.name)
-        
+        var copyEquation = [...this.state.equation]
+        copyEquation.push(e.target.name)
+        copyEquation = copyEquation.join("")
+        console.log(copyEquation)
+        var copyHistory = [...this.state.history];
+        this.updateState({equation:copyEquation,history:copyHistory})
+        // console.log(e.target.name)
+    }
+
+    handleSolve(){
+        var copyEquation = this.state.equation;
+        var copyHistory = [...this.state.history];
+        console.log("history:",this.state.history)
+        var res = string(evaluate(copyEquation))
+        copyHistory.push(copyEquation+"="+res)
+        // console.log(copyHistory)
+        this.updateState({
+            equation:res,
+            history:copyHistory
+        })
+    }
+
+    clearEquation(){
+        if (this.state.equation === ""){
+            var copyHistory = []
+        } else {
+            var copyHistory = this.state.history;
+        }
+        this.updateState({equation:"",history:copyHistory})
+    }
+
+    bsEquation(){
+        var copyEquation = [...this.state.equation]
+        copyEquation = copyEquation.slice(0,copyEquation.length-1).join("")
+        var copyHistory = this.state.history;
+        this.updateState({equation:copyEquation,history:copyHistory})
     }
 
     render(){
         return(
             <div className="main-app">
-                <input className="input" onChange={this.handleEquationChange} value={this.equation} />
+                <input className="input" value={this.state.equation} onChange={this.handleEquationChange}  />
                 <div className="numpad">
                     <button name="pi" onClick={this.handleClick}>&pi;</button>
-                    <button name="mod" onClick={this.handleClick}>|x|</button>
-                    <button name="exp" onClick={this.handleClick}>exp</button>
-                    <button name="clear" onClick={this.handleClick}>C</button>
-                    <button name="backspace" onClick={this.handleClick}>BS</button>
-                    <button name="modulus" onClick={this.handleClick}>mod</button>
+                    <button name="abs(" onClick={this.handleClick}>abs</button>
+                    <button name="exp(" onClick={this.handleClick}>exp</button>
+                    <button onClick={this.clearEquation}>C</button>
+                    <button onClick={this.bsEquation}>BS</button>
+                    <button name="sin(" onClick={this.handleClick}>sin</button>
                     <button name="(" onClick={this.handleClick}>(</button>
                     <button name=")" onClick={this.handleClick}>)</button>
-                    <button name="factorial" onClick={this.handleClick}>n!</button>
+                    <button name="factorial(" onClick={this.handleClick}>!</button>
                     <button name="/" onClick={this.handleClick}>/</button>
-                    <button name="x^y" onClick={this.handleClick}>x^y</button>
+                    <button name="cos(" onClick={this.handleClick}>cos</button>
                     <button name="7" onClick={this.handleClick}>7</button>
                     <button name="8" onClick={this.handleClick}>8</button>
                     <button name="9" onClick={this.handleClick}>9</button>
                     <button name="*" onClick={this.handleClick}>*</button>
-                    <button name="10^x" onClick={this.handleClick}>10^x</button>
+                    <button name="tan(" onClick={this.handleClick}>tan</button>
                     <button name="4" onClick={this.handleClick}>4</button>
                     <button name="5" onClick={this.handleClick}>5</button>
                     <button name="6" onClick={this.handleClick}>6</button>
                     <button name="-" onClick={this.handleClick}>-</button>
-                    <button name="log" onClick={this.handleClick}>log</button>
+                    <button name="log(" onClick={this.handleClick}>log</button>
                     <button name="1" onClick={this.handleClick}>1</button>
                     <button name="2" onClick={this.handleClick}>2</button>
                     <button name="3" onClick={this.handleClick}>3</button>
                     <button name="+" onClick={this.handleClick}>+</button>
-                    <button name="ln" onClick={this.handleClick}>ln</button>
-                    <button name="e" onClick={this.handleClick}>e</button>
+                    <button name="ln(" onClick={this.handleClick}>ln</button>
+                    <button name="sqrt(" onClick={this.handleClick}>&#8730;</button>
                     <button name="0" onClick={this.handleClick}>0</button>
                     <button name="." onClick={this.handleClick}>.</button>
-                    <button name="=" onClick={this.handleClick}>=</button>
+                    <button name="=" onClick={this.handleSolve}>=</button>
 
                 </div>
+                {/* <input type={"text"} value={this.state.history} className="history" /> */}
+                <textarea readOnly className="history" value={this.state.history.join("\n")}></textarea>
             </div>
         )
     }
